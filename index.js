e10c49305f0a15022b3f9df373f9202584bd2446
rated_demon@ukr.net <rated_demon@ukr.net>
2018-08-24 13:54:26 +0300

    fixes

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 const bodyParser = require("body-parser");
+const morgan = require('morgan');
 
 const jsonParser = bodyParser.json();
 
+app.use(morgan('tiny'));
+
 app.post('/', jsonParser, async (req, res) => {
-  if(!req.body) return res.sendStatus(400);
+  try{
+    if(!req.body) return res.sendStatus(400);
   
-  return res.json({
-    link: await puppeteer(req.body)
-  });
+    const link = await puppeteer(req.body)
+  
+    return res.json({link});
+  } catch (e){
+    return res.status(400).send({
+      error: e.message
+    });
+  }
 });
 
 app.listen(config.port, function () {
--- puppeteer.js
+++ puppeteer.js
@@ -3,55 +3,58 @@ const config = require('./config');
 
 
 module.exports = async (userData) => {
-    try{
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(`${config.taxify_link}/login`);
-        
-        await page.click('#username');
-        await page.type('#username', config.taxify_email);
-      
-        await page.click('#password');
-        await page.type('#password', config.taxify_password);
-        await page.click('form button[type=submit]');
-    
-        await page.waitForNavigation();
-        await page.click('#ember152');
-    
-        await page.click('#email')
-        await page.type('#email', userData.email);
-    
-        await page.click('#phone')
-        await page.type('#phone', userData.phone);
-    
-        await page.click('form button[type=submit]');
-    
-        await page.waitFor(2000);
-    
-        const result = await page.evaluate(() => {
-          let link = document.querySelector('.content > div > div > h4 > a').href;
-    
-          return link;
-        });
-        
-        await page.goto(result);
-    
-        await page.click('#first_name')
-        await page.type('#first_name', userData.first_name);
-    
-        await page.click('#last_name')
-        await page.type('#last_name', userData.last_name);
-    
-        await page.click('form button[type=submit]');
-    
-        await page.waitForNavigation();
-    
-        const link = page.url();
-    
-        await browser.close();
+  try{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`${config.taxify_link}/login`);
 
-        return link;
-      }catch(e){
-        console.log(e);
-      }
+    await page.click('#username');
+    await page.type('#username', config.taxify_email);
+  
+    await page.click('#password');
+    await page.type('#password', config.taxify_password);
+    await page.click('form button[type=submit]');
+
+    await page.waitForNavigation();
+    await page.click('#ember152');
+
+    await page.click('#email')
+    await page.type('#email', userData.email);
+
+    await page.click('#phone')
+    await page.type('#phone', userData.phone);
+
+    await page.click('form button[type=submit]');
+
+    await page.waitFor(2000);
+    
+    // await page.screenshot({path: 'google.png'});
+
+    const result = await page.evaluate(() => {
+      let link = document.querySelector('.content > div > div > h4 > a').href;
+
+      return link;
+    });
+    
+    await page.goto(result);
+
+    await page.click('#first_name')
+    await page.type('#first_name', userData.first_name);
+
+    await page.click('#last_name')
+    await page.type('#last_name', userData.last_name);
+
+    await page.click('form button[type=submit]');
+
+    await page.waitForNavigation();
+
+    const link = await page.url();
+
+    await browser.close();
+    
+    return link;
+  }catch(e){
+    console.log(e);
+    throw new Error(e);
+  }
 }
